493772b168927172936cec65e20528087b7d5ff4
HenrickqS <herog234@gmail.com>
2026-06-01 16:58:11 -0300

    Adicionei funcoes para os inputs e botoes entrada e saida registrarem os valores, e ajustei a responsividade para celulares pequenos, medios e tablets

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-const 
+const elementoSaldo = document.getElementById('saldo-atual');
+const elementoEntrada = document.getElementById('saldo-entradas');
+const elementoSaida = document.getElementById('saldo-saidas');
+const descricao = document.getElementById('input-descricao');
+const valor = document.getElementById('input-valor');
+const btnEntrada = document.getElementById('btn-entrada');
+const btnSaida = document.getElementById('btn-saida');
+
+let saldoTotal = 0.00;
+let saldoEntrada = 0.00;
+let saldoSaida = 0.00;
+
+btnEntrada.addEventListener('click', function(){
+  
+  const adicionarSaldo = Number(valor.value);
+
+  saldoTotal = saldoTotal+adicionarSaldo;
+  saldoEntrada = saldoEntrada+adicionarSaldo;
+
+  elementoSaldo.innerText = saldoTotal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+  elementoEntrada.innerText = saldoEntrada.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+
+});
+
+btnSaida.addEventListener('click', function(){
+  
+  const DiminuirSaldo = Number(valor.value);
+
+  saldoTotal = saldoTotal-DiminuirSaldo;
+  saldoSaida = saldoSaida+DiminuirSaldo;
+
+  elementoSaldo.innerText = saldoTotal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+  elementoSaida.innerText = saldoSaida.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+
+});
