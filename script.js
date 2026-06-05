fb94e791ac7214dc0a0d5c6acca63d692a32f809
HenrickqS <herog234@gmail.com>
2026-06-05 17:43:29 -0300

    agora o sistema pode ser usado no localstorage. VERSAO V1 OFICIALMENTE PRONTA

--- script.js
+++ script.js
@@ -12,8 +12,35 @@ let saldoEntrada = 0.00;
 let saldoSaida = 0.00;
 let ultimasTransacoes = [];
 
+
+const transacoesSalvas = localStorage.getItem('transacoes');
+
+if (transacoesSalvas) {
+    ultimasTransacoes = JSON.parse(transacoesSalvas);
+
+    ultimasTransacoes.forEach(function(transacao) {
+        adicionarAoHistorico(transacao.descricao, transacao.valor, transacao.tipo);
+        
+        const valorLimpo = transacao.valor.replace(/\D/g, '');
+        const valorNumero = Number(valorLimpo) / 100;
+        
+        if (transacao.tipo === 'entrada') {
+            saldoTotal += valorNumero;
+            saldoEntrada += valorNumero;
+        } else {
+            saldoTotal -= valorNumero;
+            saldoSaida += valorNumero;
+        }
+    });
+    
+
+    elementoSaldo.innerText = saldoTotal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    elementoEntrada.innerText = saldoEntrada.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    elementoSaida.innerText = saldoSaida.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+}
+
+
 btnEntrada.addEventListener('click', function(){
-  
   const valorLimpo = valor.value.replace(/\D/g, '');
   const adicionarSaldo = Number(valorLimpo) / 100;
 
@@ -22,65 +49,74 @@ btnEntrada.addEventListener('click', function(){
     return;
   }
 
-  saldoTotal = saldoTotal+adicionarSaldo;
-  saldoEntrada = saldoEntrada+adicionarSaldo;
+  saldoTotal = saldoTotal + adicionarSaldo;
+  saldoEntrada = saldoEntrada + adicionarSaldo;
 
   elementoSaldo.innerText = saldoTotal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
   elementoEntrada.innerText = saldoEntrada.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+
+
+  ultimasTransacoes.push({
+    id: Date.now(),
+    descricao: descricao.value,
+    valor: valor.value,
+    tipo: 'entrada'
+  });
+  localStorage.setItem('transacoes', JSON.stringify(ultimasTransacoes));
 
   adicionarAoHistorico(descricao.value, valor.value, 'entrada');
 
   descricao.value = '';
   valor.value = '';
   descricao.focus();
-
-
 });
 
+
 btnSaida.addEventListener('click', function(){
-  
   const valorLimpo = valor.value.replace(/\D/g, '');
   const DiminuirSaldo = Number(valorLimpo) / 100;
 
-    if (descricao.value.trim() === '' || valor.value.trim() === '') {
+  if (descricao.value.trim() === '' || valor.value.trim() === '') {
     alert('Por favor, preencha a descrição e o valor!');
     return;
-  };
+  }
 
-  saldoTotal = saldoTotal-DiminuirSaldo;
-  saldoSaida = saldoSaida+DiminuirSaldo;
+  saldoTotal = saldoTotal - DiminuirSaldo;
+  saldoSaida = saldoSaida + DiminuirSaldo;
 
   elementoSaldo.innerText = saldoTotal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
   elementoSaida.innerText = saldoSaida.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
   
+
+  ultimasTransacoes.push({
+    id: Date.now(),
+    descricao: descricao.value,
+    valor: valor.value,
+    tipo: 'saida'
+  });
+  localStorage.setItem('transacoes', JSON.stringify(ultimasTransacoes));
+
   adicionarAoHistorico(descricao.value, valor.value, 'saida');
   
   descricao.value = '';
   valor.value = '';
   descricao.focus();
-
-
 });
 
-valor.addEventListener('input', function() {
-   
-  let apenasNumeros = valor.value.replace(/\D/g, '');
 
+valor.addEventListener('input', function() {
+  let apenasNumeros = valor.value.replace(/\D/g, '');
   if (apenasNumeros === '') {
     valor.value = '';
     return;
   }
-
   let valorNumero = Number(apenasNumeros) / 100;
-
   let valorFormatado = valorNumero.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-
   valor.value = valorFormatado;
-
 });
 
- function adicionarAoHistorico(textoDescricao, textoValor, tipo) {
-    
+
+function adicionarAoHistorico(textoDescricao, textoValor, tipo) {
     const novoItem = document.createElement('li');
 
     let seta = tipo === 'entrada' ? '<i data-lucide="arrow-up-right"></i>' : '<i data-lucide="arrow-down-left"></i>';
@@ -89,8 +125,7 @@ valor.addEventListener('input', function() {
 
     novoItem.innerHTML = 
       `<div class="coluna-esquerda">
-      <div class = "circulo-seta">${seta}
-      </div>
+      <div class = "circulo-seta">${seta}</div>
       <span class='descricao-texto'>${textoDescricao}</span>
       </div>
       <span class='tipo-texto'>${tipoFormatado}</span>
@@ -98,42 +133,40 @@ valor.addEventListener('input', function() {
       <span class=valor-texto>${sinal} ${textoValor}</span>
       <button class="btn-deletar"><i data-lucide="trash-2"></i></button>
       </div>`;
+      
     novoItem.classList.add('item-transacao');
 
     if (tipo === 'entrada') {
       novoItem.classList.add('cor-entrada');
-    }
-    else {
+    } else {
       novoItem.classList.add('cor-saida');
     }
 
     historico.appendChild(novoItem);
     lucide.createIcons();
-
-    ultimasTransacoes.push({
-    elementoHtml: novoItem,
-    valorNumerico: Number(textoValor.replace(/\D/g, '')) / 100,
-    tipoTransacao: tipo
-  });
   
-  const btndeletar = novoItem.querySelector('.btn-deletar')
+    const btndeletar = novoItem.querySelector('.btn-deletar');
 
-  btndeletar.addEventListener('click', function() {
-    novoItem.remove();
+    btndeletar.addEventListener('click', function() {
+        novoItem.remove();
 
-    const valorNumerico = Number(textoValor.replace(/\D/g, '')) / 100;
+        const valorNumerico = Number(textoValor.replace(/\D/g, '')) / 100;
 
-    if (tipo === 'entrada') {
-      saldoTotal = saldoTotal - valorNumerico;
-      saldoEntrada = saldoEntrada - valorNumerico;
-    }
-    else {
-      saldoTotal = saldoTotal + valorNumerico;
-      saldoSaida = saldoSaida - valorNumerico;
-    }
+        if (tipo === 'entrada') {
+          saldoTotal = saldoTotal - valorNumerico;
+          saldoEntrada = saldoEntrada - valorNumerico;
+        } else {
+          saldoTotal = saldoTotal + valorNumerico;
+          saldoSaida = saldoSaida - valorNumerico;
+        }
 
-    elementoSaldo.innerText = saldoTotal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-    elementoEntrada.innerText = saldoEntrada.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-    elementoSaida.innerText = saldoSaida.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-  })
-  }
+        elementoSaldo.innerText = saldoTotal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+        elementoEntrada.innerText = saldoEntrada.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+        elementoSaida.innerText = saldoSaida.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+
+        ultimasTransacoes = ultimasTransacoes.filter(function(transacao) {
+            return transacao.descricao !== textoDescricao || transacao.valor !== textoValor;
+        });
+        localStorage.setItem('transacoes', JSON.stringify(ultimasTransacoes));
+    });
+}
